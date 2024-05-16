a7eb1066a4cff103bb4fd0a237da8a787f16f834
Gilson Azevedo de Melo <gamelo@fazenda.net>
2024-05-16 17:08:23 -0300

    Melhorias nos estilos dos formulários e métodos para tableviews(ajustes)3.

--- src/main/java/com/example/dechivejavafx/gui/util/Configuracao.java
+++ src/main/java/com/example/dechivejavafx/gui/util/Configuracao.java
@@ -3,15 +3,15 @@ package com.example.dechivejavafx.gui.util;
 import com.example.dechivejavafx.Validacoes.ConnectionStatus;
 
 public class Configuracao {
-    public static int dias = 20;
+    public static int dias = 30;
     public static int minutoInicial =59;
     public static int intervaloHora = 6;
     public static int diasSped = 20;
     public static int diasSpedHive = diasSped + 1;
     public static int diasSpedRecuo = 2;
-    public static int inicioDuplicidade = 2024010100;
+    public static int inicioDuplicidade = 2024010130;
     public static int tamanhoLote = 30000;
-    public static long esperaQuery = 15;
+    public static long esperaQuery = 0;
     public static ConnectionStatus connectionOraprd21Status = ConnectionStatus.ENABLED;
     public static String enumDocumentoPrincipal = "sim";
     public static String enumDocumentoCancelamento = "sim";
